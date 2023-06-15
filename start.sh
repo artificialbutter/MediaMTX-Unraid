fae74dc29a72d0ea4e6f0aff2f3aa359b57005f3
artificial <59352535+artificialbutter@users.noreply.github.com>
2023-06-15 15:09:58 -0700

    make the start script fancier

--- start.sh
+++ start.sh
@@ -4,6 +4,16 @@ file_path="/app/config/mediamtx.xml"
 link_path="/app/mediamtx.yml"
 url="https://raw.githubusercontent.com/bluenviron/mediamtx/v0.23.5/mediamtx.yml"
 
+
+echo " __  __          _ _       __  __ _______  __"
+echo "|  \/  | ___  __| (_) __ _|  \/  |_   _\ \/ /"
+echo "| |\/| |/ _ \/ _` | |/ _` | |\/| | | |  \  / "
+echo "| |  | |  __/ (_| | | (_| | |  | | | |  /  \ "
+echo "|_|  |_|\___|\__,_|_|\__,_|_|  |_| |_| /_/\_\\"
+
+echo "Image by artificialbutter <3"
+echo "=============================="
+
 if [ ! -f "$file_path" ]; then
   echo "Config not found. Downloading config..."
   curl -o "$file_path" "$url" -s
